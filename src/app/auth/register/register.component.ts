1c261a4fc8a49b383e50570cba1b6950a259c788
Stanciu <MStanciu@romania.luxoft.com>
2017-07-10 17:32:42 +0300

    form validator temporary

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  @ViewChild('f') signUp: NgForm;
+  answer = '';
+  genders = ['male','female'];
 
   constructor() { }
 
   ngOnInit() {
+    
+  }
+
+  onSubmit(form: NgForm) {
+    //console.log('submitted');
+    //console.log(form.value);
+    console.log(this.signUp);
   }
 
 }
